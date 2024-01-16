652c332cab0b236095c4ae81a56e214004bb9f48
Taeri <taesaa@ttu.ee>
2024-01-16 20:46:23 +0200

    revert

--- word-cloud/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import 'jquery';
-import '@popperjs/core';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import 'font-awesome/css/font-awesome.min.css';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
